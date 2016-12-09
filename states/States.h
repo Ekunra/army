134cd6b7a488825dbe70ad002b648b96aa2a8b02
Ekunra <ba.m.regs@gmail.com>
2016-12-09 20:06:18 +0200

    [unit] Integrated StateEnum into States class

--- states/States.h
+++ states/States.h
@@ -2,6 +2,7 @@
 #define STATES_H
 
 #include <iostream>
+#include "StateEnum.cpp"
 #include "../Exceptions.h"
 
 class States {
@@ -9,18 +10,20 @@ class States {
         int hitPoints;
         int hitPointsLimit;
         int damage;
+        StateEnum uEnum;
         std::string title;
 
         void applyDamage(int pain);
 
     public:
-        States(int hp, int damage, const std::string& title="Default");
+        States(int hp, int damage, const std::string& title="Default", StateEnum uEnum=ZERO);
         virtual ~States();
 
         const int getHitPoints() const;
         const int getHitPointsLimit() const;
         const int getDamage() const;
         const std::string& getTitle() const;
+        const StateEnum getUEnum() const;
 
         virtual void takeDamage(States* enemy);
         virtual void takeCADamage(States* enemy);
