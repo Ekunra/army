27447559a4719e29d4393232da14cb7aa7351e48
ekunra <ba.m.regs@gmail.com>
2016-12-28 22:03:45 +0200

    [ArmyDev] Edited limitedfield class

--- limitedfield/LimitedField.h
+++ limitedfield/LimitedField.h
@@ -3,13 +3,11 @@
 
 #include <iostream>
 
-template<class Type>
+template <class Type>
 class LimitedField {
     protected:
         Type* value;
         Type* limit;
-
-        Type validate(Type result);
 
     public:
         LimitedField(Type value);
@@ -17,48 +15,9 @@ class LimitedField {
 
         const Type getValue() const;
         const Type getLimit() const;
-
-        void show(const LimitedField* other);
-
-        const bool operator==(const LimitedField& other) const;
-        const bool operator!=(const LimitedField& other) const;
-        const bool operator>=(const LimitedField& other) const;
-        const bool operator<=(const LimitedField& other) const;
-        const bool operator>(const LimitedField& other) const;
-        const bool operator<(const LimitedField& other) const;
-
-        void operator+=(const LimitedField& other);
-        void operator-=(const LimitedField& other);
-        void operator*=(const LimitedField& other);
-        void operator/=(const LimitedField& other);
-
-        Type operator+(const LimitedField& other);
-        Type operator-(const LimitedField& other);
-        Type operator*(const LimitedField& other);
-        Type operator/(const LimitedField& other);
-
-        const bool operator==(Type value) const;
-        const bool operator!=(Type value) const;
-        const bool operator>=(Type value) const;
-        const bool operator<=(Type value) const;
-        const bool operator>(Type value) const;
-        const bool operator<(Type value) const;
-
-        void operator+=(Type value);
-        void operator-=(Type value);
-        void operator*=(Type value);
-        void operator/=(Type value);
-
-        Type operator+(Type value);
-        Type operator-(Type value);
-        Type operator*(Type value);
-        Type operator/(Type value);
-
-        LimitedField operator=(const LimitedField& other);
-        LimitedField operator=(Type value);
 };
 
-template<class Type>
+template <class Type>
 std::ostream& operator<<(std::ostream& out, const LimitedField<Type>& field);
 
 #endif // LIMITEDFIELD_H
--- limitedfield/main.cpp
+++ limitedfield/main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include "LimitedField.h"
 
+
+class Test {
+    protected:
+        LimitedField<double>* lfield;
+    public:
+        Test(double value) : lfield(new LimitedField<double>(value)) {}
+        ~Test() {
+            delete lfield;
+        }
+
+        void show() {
+            std::cout << *lfield << std::endl;
+        }
+};
+
 int main() {
-	LimitedField<int>* f1 = new LimitedField<int>(100);
-	LimitedField<int>* f2 = new LimitedField<int>(100);
+    Test* test = new Test(100.5);
+    LimitedField<int>* f1 = new LimitedField<int>(100);
+    
+    std::cout << *f1 << std::endl;
+    test->show();
 
-
-	std::cout << *f1 << std::endl;
-	std::cout << *f2 << std::endl;
-
-	if ( f1 == f2 ) {
-		std::cout << "Yes" << std::endl;
-	} else {
-		std::cout << "NO" << std::endl;
-
-	}
-
-	f1->show(f2);
-
-	std::cout << f1->getValue() << std::endl;
-	std::cout << f1->getLimit() << std::endl;
-
-
-	delete f1;
-	delete f2;
-	return 0;
+    delete f1;
+    delete test;
+    return 0;
 }
