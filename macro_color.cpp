2e166bcf2746d4a45f203935f7ff14588b2572b2
ekunra <ba.m.regs@gmail.com>
2017-01-10 02:25:16 +0200

    [ArmyDev] Add macro_color.cpp with color definition MACROSes

---
+++ macro_color.cpp
@@ -0,0 +1,35 @@
+#ifndef MACRO_COLOR_CPP
+#define MACRO_COLOR_CPP
+
+#define FO_B "\033[1m"
+
+#define FO_BLACK "\033[30m"
+#define FO_RED "\033[31m"
+#define FO_GREEN "\033[32m"
+#define FO_YELLOW "\033[33m"
+#define FO_BLUE "\033[34m"
+#define FO_VIOLET "\033[35m"
+#define FO_LBLUE "\033[36m"
+#define FO_GREY "\033[37m"
+
+#define FO_B_BLACK "\033[1;30m"
+#define FO_B_RED "\033[1;31m"
+#define FO_B_GREEN "\033[1;32m"
+#define FO_B_YELLOW "\033[1;33m"
+#define FO_B_BLUE "\033[1;34m"
+#define FO_B_VIOLET "\033[1;35m"
+#define FO_B_LBLUE "\033[1;36m"
+#define FO_B_GREY "\033[1;37m"
+
+#define FO_D_BLACK "\033[2;30m"
+#define FO_D_RED "\033[2;31m"
+#define FO_D_GREEN "\033[2;32m"
+#define FO_D_YELLOW "\033[2;33m"
+#define FO_D_BLUE "\033[2;34m"
+#define FO_D_VIOLET "\033[2;35m"
+#define FO_D_LBLUE "\033[2;36m"
+#define FO_D_GREY "\033[2;37m"
+
+#define FO_RESET "\033[0m"
+
+#endif // MACRO_COLOR_CPP
