d6c865fbf6c5dadfbab90720fa4453d62124affc
Ekunra <ba.m.regs@gmail.com>
2017-06-21 00:49:56 +0300

    [ArmyDev] Trying to commit whit the correct Name

--- observe/Observer.cpp
+++ observe/Observer.cpp
@@ -1,7 +1,7 @@
 #include "Observer.h"
 
 template <class Type>
-Observer<Type>::Observer(Necromancer<Type>* necr) : owner(necr) {
+Observer<Type>::Observer(Necromancer<Type>* necr) : owner(necr), observables(new std::set<Unit<Type>*>) {
     if ( DEBUG ) {
         std::cout << FO_B_GREEN << "|        + " << FO_RESET;
         std::cout << FO_B << "Observer" << FO_RESET << " instance created. " << std::endl;
